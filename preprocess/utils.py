c3a3437499c5ceabaa0c474b9064be93de0066e4
Zihan Wang <wzhyh@outlook.com>
2023-09-18 10:10:12 +0800

    Add files via upload

---
+++ preprocess/utils.py
@@ -0,0 +1,15 @@
+import os
+import json
+import time
+
+def load_viewpoint_ids(connectivity_dir):
+    viewpoint_ids = []
+    with open(os.path.join(connectivity_dir, 'scans.txt')) as f:
+        scans = [x.strip() for x in f]
+    for scan in scans:
+        with open(os.path.join(connectivity_dir, '%s_connectivity.json'%scan)) as f:
+            data = json.load(f)
+            viewpoint_ids.extend([(scan, x['image_id']) for x in data if x['included']])
+    print('Loaded %d viewpoints' % len(viewpoint_ids))
+    return viewpoint_ids
+
