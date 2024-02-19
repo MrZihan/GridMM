57293fe10857cb1803fb0d07ac5b8d7135342408
Zihan Wang <wzhyh@outlook.com>
2024-02-19 19:51:02 +0800

    Update run_GridMap.bash

--- VLN_CE/run_GridMap.bash
+++ VLN_CE/run_GridMap.bash
@@ -15,19 +15,19 @@
 
 
 # # TRAINING (Single node multiple GPUs)
-# flag="--exp_name cont-cwp-gridmap-ori
+#flag="--exp_name cont-cwp-gridmap-ori
 #       --run-type train
 #       --exp-config run_GridMap.yaml
-#       GPU_NUMBERS 4
-#       SIMULATOR_GPU_IDS [0,1,2,3]
-#       TORCH_GPU_IDS [0,1,2,3]
-#       IL.batch_size 2
+#       GPU_NUMBERS 2
+#       SIMULATOR_GPU_IDS [0,1]
+#       TORCH_GPU_IDS [0,1]
+#       IL.batch_size 4
 #       IL.lr 1e-5
-#       IL.epochs 30
+#       IL.epochs 100
 #       IL.schedule_ratio 0.50
 #       IL.decay_time 20
 #       "
-#CUDA_VISIBLE_DEVICES='4,5,6,7' python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=29503 run.py $flag
+#CUDA_VISIBLE_DEVICES='6,7' python3 -m torch.distributed.launch --nproc_per_node=2 --master_port=29503 run.py $flag
 
 
 # # EVALUATION
